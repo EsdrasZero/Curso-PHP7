1fe1c5b25a54f663857b721b966d3cc070a872b5
Esdras Mendes <106601091+EsdrasZero@users.noreply.github.com>
2024-01-12 14:49:09 -0300

    Hello PHP

---
+++ hello.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php 
+            echo '<p>Hello PHP</p>'; 
+            echo '<p>What a wonderful day</p>' 
+        ?>
+    </body>
+</html>
