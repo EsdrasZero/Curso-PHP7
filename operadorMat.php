bcb418cd51faf37c3cd0381b1797d79a544e5d33
Esdras Mendes <esdrasmendes0@outlook.com>
2024-01-12 16:01:42 -0300

    Operadores matematicos

---
+++ operadorMat.php
@@ -0,0 +1,27 @@
+<?php
+$soma = 2 + 2;
+$subtração = 2 - 3;
+$mutiplicacao = 2 * 2;
+$divisao = 2 / 2;
+$modulo = 2 % 2;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>2 + 2 = <?= $soma ?></p>
+    <p>2 - 3 = <?= $subtração ?></p>
+    <p> 2 * 2 =<? $mutiplicacao ?></p>
+    <p>2 / 2 = <? $divisao ?></p>
+    <p> 2 % 2 = <? $modulo ?></p>
+
+</body>
+
+</html>
